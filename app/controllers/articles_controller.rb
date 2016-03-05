f7275ed1d81a50e4192b8d1a62736282b8cadfc1
Matthew Garsteck <g-photography@hotmail.com>
2016-03-05 23:47:19 +0000

    Add: update, show, edit, index

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,19 @@
 class ArticlesController < ApplicationController
   
+  def index
+    @articles = Article.all
+  end
+  
+  
   def new
     @article = Article.new 
   end
+  
+  
+  def edit
+    @article = Article.find(params[:id ])
+  end
+  
   
   def create
 
@@ -16,6 +27,19 @@ class ArticlesController < ApplicationController
       render :new
     end
   end
+  
+  
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "_-_ MY LORD, YOUR ARTICLE WAS SUCCESSFULLY UPDATED _-_"
+      redirect_to article_path(@article)
+    else
+      render :edit
+    end
+    
+  end
+  
   
   def show
     
